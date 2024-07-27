7cd384f8ef8686abe3ad05e7a092d4f6c196b3d1
Tiffany Cai <tiffanycai.vv@gmail.com>
2024-07-27 19:06:36 -0400

    gmail api and readme gmail api updated and secrets deleted

--- sendEmail.py
+++ sendEmail.py
@@ -12,7 +12,7 @@ def send_email(subject, body):
     service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
     mimeMessage = MIMEMultipart()
-    mimeMessage['to'] = 'tiffany.tc05@gmail.com'
+    mimeMessage['to'] = '<Receipient>@gmail.com'
     mimeMessage['subject'] = subject
     mimeMessage.attach(MIMEText(body, 'plain'))
     raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
